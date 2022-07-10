9faf64635858de84fb2cafa2aa44bc4777e6e298
udacity-tom <70481612+udacity-tom@users.noreply.github.com>
2022-07-10 17:56:04 +0200

    Chore: update EB URL for app

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost: 'http://api-environment.eba-ijh65xvf.us-east-1.elasticbeanstalk.com/api/v0',
+  // apiHost: 'http://localhost:8080/api/v0',
 };
 
 /*
